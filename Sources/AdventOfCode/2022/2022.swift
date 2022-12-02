91e682fd59ae3d55aac7fad26934905a51069934
Eddy Gammon <eddy@multicolourpixel.com>
2022-12-02 09:18:54 +0000

    Add 2022 Day 2

--- Sources/AdventOfCode/2022/2022.swift
+++ Sources/AdventOfCode/2022/2022.swift
@@ -61,6 +61,92 @@ struct AdventOfCode2022 {
         
         return maxCalories
     }
+    
+    func day2_1() -> Int {
+        let allRounds: [String] = loadFile()
+        return allRounds.map { game in
+            let round = game.split(separator: " ")
+                .compactMap(RockPaperScissors.init)
+            
+            return RockPaperScissors.matchResult(round[0], me: round[1]) + round[1].points
+        }
+        .reduce(0, +)
+    }
+    
+    func day2_2() -> Int {
+        let allRounds: [String] = loadFile()
+        return allRounds.map { game in
+            let splits = game.split(separator: " ")
+            
+            let opponentMove = RockPaperScissors(rawValue: splits[0])!
+            let desiredOutcome = RockPaperScissors.DesiredOutcome(rawValue: splits[1])!
+            let outcomeMove = opponentMove.moveForDesiredOutcome(desiredOutcome)
+            
+            return RockPaperScissors.matchResult(opponentMove, me: outcomeMove) + outcomeMove.points
+        }
+        .reduce(0, +)
+    }
 }
 
 extension AdventOfCode2022: FileLoading { }
+
+private extension AdventOfCode2022 {
+    enum RockPaperScissors: Substring {
+        enum DesiredOutcome: Substring {
+            case lose = "X"
+            case draw = "Y"
+            case win = "Z"
+        }
+        
+        case rock
+        case paper
+        case scissors
+        
+        var points: Int {
+            switch self {
+            case .rock: return 1
+            case .paper: return 2
+            case .scissors: return 3
+            }
+        }
+        
+        init?(rawValue: Substring) {
+            if rawValue == "A" || rawValue == "X" {
+                self = .rock
+            }
+            else if rawValue == "B" || rawValue == "Y" {
+                self = .paper
+            }
+            else if rawValue == "C" || rawValue == "Z" {
+                self = .scissors
+            }
+            else {
+                return nil
+            }
+        }
+        
+        static func matchResult(_ opponent: Self, me: Self) -> Int {
+            switch (opponent, me) {
+            case (.rock, .rock): return 3
+            case (.rock, .paper): return 6
+            case (.paper, .paper): return 3
+            case (.paper, .scissors): return 6
+            case (.scissors, .scissors): return 3
+            case (.scissors, .rock): return 6
+            default: return 0
+            }
+        }
+        
+        func moveForDesiredOutcome(_ desiredOutcome: DesiredOutcome) -> Self {
+            switch (self, desiredOutcome) {
+            case (.rock, .win): return .paper
+            case (.rock, .lose): return .scissors
+            case (.paper, .win): return .scissors
+            case (.paper, .lose): return .rock
+            case (.scissors, .win): return .rock
+            case (.scissors, .lose): return .paper
+            default: return self
+            }
+        }
+    }
+}
--- Tests/AdventOfCodeTests/AdventOfCode2022Tests.swift
+++ Tests/AdventOfCodeTests/AdventOfCode2022Tests.swift
@@ -16,4 +16,16 @@ final class AdventOfCode2022Tests: XCTestCase {
     func testDay1_part2() {
         print(#function + ": \(advent.day1_2())")
     }
+    
+    func testDay2_part1() {
+        let result = advent.day2_1()
+        XCTAssertEqual(result, 11475)
+        print(#function + ": \(advent.day2_1())")
+    }
+    
+    func testDay2_part2() {
+        let result = advent.day2_2()
+        XCTAssertEqual(result, 16862)
+        print(#function + ": \(result)")
+    }
 }
