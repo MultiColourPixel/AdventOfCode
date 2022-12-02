5f66cd091c771016c8502e77c2b06793efd64866
Eddy Gammon <eddy@multicolourpixel.com>
2022-12-02 09:18:09 +0000

    Add 2022 files with Day 1 challenges completed

---
+++ Sources/AdventOfCode/2022/2022.swift
@@ -0,0 +1,66 @@
+//
+//  Created by Eddy Gammon on 01/12/2022.
+//
+
+import Algorithms
+import Foundation
+
+struct AdventOfCode2022 {
+    func day1_1() -> Int {
+        let input: [Int?] = loadFile(omittingEmptySubsequences: false)
+        
+        var maxCalories = 0
+        var currentElfCalories = 0
+        for calorie in input {
+            guard let calorie else {
+                maxCalories = max(maxCalories, currentElfCalories)
+                currentElfCalories = 0
+                continue
+            }
+
+            currentElfCalories += calorie
+        }
+
+        return maxCalories
+    }
+    
+    func day1_2() -> Int {
+        let input: [Int?] = loadFile(omittingEmptySubsequences: false)
+        
+        var currentElfCalories = 0
+        var topElf = 0
+        var secondElf = 0
+        var thirdElf = 0
+        
+        for calorie in input {
+            guard let calorie else {
+                defer { currentElfCalories = 0 }
+                
+                let minElf = min(topElf, secondElf, thirdElf)
+                guard currentElfCalories > minElf else { continue }
+                
+                if topElf == minElf {
+                    topElf = currentElfCalories
+                    continue
+                }
+                if secondElf == minElf {
+                    secondElf = currentElfCalories
+                    continue
+                }
+                if thirdElf == minElf {
+                    thirdElf = currentElfCalories
+                    continue
+                }
+                continue
+            }
+            
+            currentElfCalories += calorie
+        }
+        
+        let maxCalories = topElf + secondElf + thirdElf
+        
+        return maxCalories
+    }
+}
+
+extension AdventOfCode2022: FileLoading { }
---
+++ Tests/AdventOfCodeTests/AdventOfCode2022Tests.swift
@@ -0,0 +1,19 @@
+//
+//  Created by Eddy Gammon on 01/12/2022.
+//
+
+import XCTest
+
+@testable import AdventOfCode
+
+final class AdventOfCode2022Tests: XCTestCase {
+    let advent = AdventOfCode2022()
+    
+    func testDay1_part1() {
+        print(#function + ": \(advent.day1_1())")
+    }
+    
+    func testDay1_part2() {
+        print(#function + ": \(advent.day1_2())")
+    }
+}
