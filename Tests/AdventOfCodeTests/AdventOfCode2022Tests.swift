df0c8d77032b5b5a26d9cbe4d900acef0515a1ee
Eddy Gammon <eddy@multicolourpixel.com>
2022-12-04 16:04:33 +0000

    Add `measure` for all of the test cases so far in 2022

--- Tests/AdventOfCodeTests/AdventOfCode2022Tests.swift
+++ Tests/AdventOfCodeTests/AdventOfCode2022Tests.swift
@@ -10,45 +10,73 @@ final class AdventOfCode2022Tests: XCTestCase {
     let advent = AdventOfCode2022()
     
     func testDay1_part1() {
-        print(#function + ": \(advent.day1_1())")
+        var result: Int = 0
+        measure {
+            result = advent.day1_1()
+        }
+        XCTAssertEqual(result, 69289)
+        print(#function + ": \(result)")
     }
     
     func testDay1_part2() {
-        print(#function + ": \(advent.day1_2())")
+        var result: Int = 0
+        measure {
+            result = advent.day1_2()
+        }
+        XCTAssertEqual(result, 205615)
+        print(#function + ": \(result)")
     }
     
     func testDay2_part1() {
-        let result = advent.day2_1()
+        var result: Int = 0
+        measure {
+            result = advent.day2_1()
+        }
         XCTAssertEqual(result, 11475)
         print(#function + ": \(advent.day2_1())")
     }
     
     func testDay2_part2() {
-        let result = advent.day2_2()
+        var result: Int = 0
+        measure {
+            result = advent.day2_2()
+        }
         XCTAssertEqual(result, 16862)
         print(#function + ": \(result)")
     }
     
     func testDay3_part1() {
-        let result = advent.day3_1()
+        var result: Int = 0
+        measure {
+            result = advent.day3_1()
+        }
         XCTAssertEqual(result, 8088)
         print(#function + ": \(result)")
     }
     
     func testDay3_part2() {
-        let result = advent.day3_2()
+        var result: Int = 0
+        measure {
+            result = advent.day3_2()
+        }
         XCTAssertEqual(result, 2522)
         print(#function + ": \(result)")
     }
     
     func testDay4_part1() {
-        let result = advent.day4_1()
+        var result = 0
+        measure {
+            result = advent.day4_1()
+        }
         XCTAssertEqual(result, 496)
         print(#function + ": \(result)")
     }
     
     func testDay4_part2() {
-        let result = advent.day4_2()
+        var result: Int = 0
+        measure {
+            result = advent.day4_2()
+        }
         XCTAssertEqual(result, 847)
         print(#function + ": \(result)")
     }
